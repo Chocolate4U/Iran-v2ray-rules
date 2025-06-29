7692cf03db97338b5731ca592b6fb898aa1ca327
Chocolate4U <Chocolate4U@tuta.io>
2025-06-29 23:23:03 +0330

    fix the build failure
    Filter out the invalid domain to fix the build failure.

--- scripts/generate-phishing-domains-ips.sh
+++ scripts/generate-phishing-domains-ips.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gh api https://api.github.com/repos/curbengh/phishing-filter/contents/phishing-filter-dnscrypt-blocked-names.txt?ref=gh-pages -H "Accept: application/vnd.github.raw" | sed -e 's/^\(|\|@\|\*\|\.\|\-\|0\.0\.0\.0\|127\.0\.0\.1\)*//g' -e 's/\^.*$//g' -e '/!\|?\|@\|#\|\*\|_\|\\\|\/\|\[\|]\|\[\|\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}/d' -e '/\.$/d' -e '/^\s*$/d' | awk '{$1=$1};1' | dos2unix | idn2 --no-alabelroundtrip --no-tr46 | LC_ALL=C sort -u > phishing-temp.txt
+gh api https://api.github.com/repos/curbengh/phishing-filter/contents/phishing-filter-dnscrypt-blocked-names.txt?ref=gh-pages -H "Accept: application/vnd.github.raw" | sed -e 's/^\(|\|@\|\*\|\.\|\-\|0\.0\.0\.0\|127\.0\.0\.1\)*//g' -e 's/\^.*$//g' -e '/!\|?\|@\|#\|\*\|_\|\\\|\/\|\[\|]\|\[\|\([0-9]\{1,3\}\.\)\{3\}[0-9]\{1,3\}/d' -e '/\.$/d' -e '/^\s*$/d' -e '/blob:https:/d' | awk '{$1=$1};1' | dos2unix | idn2 --no-alabelroundtrip --no-tr46 | LC_ALL=C sort -u > phishing-temp.txt
 gh api https://api.github.com/repos/curbengh/phishing-filter/contents/phishing-filter-dnscrypt-blocked-ips.txt?ref=gh-pages -H "Accept: application/vnd.github.raw" | sed -e '/#/d' -e '/^$/d' > phishing-ip.txt
 [[ -s phishing-ip.txt ]] || gh api https://api.github.com/repos/Chocolate4U/Iran-v2ray-rules/contents/text/phishing.txt?ref=release -H "Accept: application/vnd.github.raw" > phishing-ip.txt
 sed -e 's/^/\./' -e 's/\./\\./g' -e 's/\-/\\-/g' -e 's/$/\$/' phishing-temp.txt > phishing-sub.txt
